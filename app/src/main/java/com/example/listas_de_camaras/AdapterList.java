604649b15368ce5c51dc0309c742dfd90bc24d0a
carloscruz05 <carloscruzfotografo1@gmail.com>
2024-06-11 22:17:09 -0600

    lista 2

--- app/src/main/java/com/example/listas_de_camaras/AdapterList.java
+++ app/src/main/java/com/example/listas_de_camaras/AdapterList.java
@@ -44,3 +44,4 @@ public class AdapterList extends ArrayAdapter<String> {
         return rowView;
     }
 }
+
--- app/src/main/java/com/example/listas_de_camaras/MainActivity.java
+++ app/src/main/java/com/example/listas_de_camaras/MainActivity.java
@@ -7,8 +7,6 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import com.example.listas_de_camaras.R;
-
 public class MainActivity extends AppCompatActivity {
     private ListView listView;
     private int total = 0;
@@ -20,8 +18,8 @@ public class MainActivity extends AppCompatActivity {
 
         listView = findViewById(R.id.List);
 
-        String[] nombres = {"Canon EOS Rebel T7", "Nikon D3500", "Sony Alpha A6000", "Fujifilm X-T30", "Olympus OM-D E-M10 Mark IV"};
-        String[] precios = {"500", "400", "600", "800", "700"};
+        String[] nombres = getResources().getStringArray(R.array.camera_names);
+        String[] precios = getResources().getStringArray(R.array.camera_prices);
         Integer[] idimage = {R.drawable.canon, R.drawable.nikon, R.drawable.sony, R.drawable.fujifilm, R.drawable.olympus};
 
         com.listas_de_camaras.AdapterList adapter = new com.listas_de_camaras.AdapterList(this, nombres, precios, idimage);
@@ -32,7 +30,8 @@ public class MainActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 int precio = Integer.parseInt(precios[position]);
                 total += precio;
-                Toast.makeText(getApplicationContext(), "Seleccionaste la cámara: " + nombres[position] + ". Total: $" + total, Toast.LENGTH_LONG).show();
+                String message = getString(R.string.camera_selected, nombres[position], total);
+                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
             }
         });
 
@@ -41,7 +40,8 @@ public class MainActivity extends AppCompatActivity {
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
                 int precio = Integer.parseInt(precios[position]);
                 total -= precio;
-                Toast.makeText(getApplicationContext(), "Removiste la cámara: " + nombres[position] + ". Total: $" + total, Toast.LENGTH_LONG).show();
+                String message = getString(R.string.camera_removed, nombres[position], total);
+                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
                 return true;
             }
         });
